c2ce5c32a77f113286a18b5d109cd8cf91c3f4be
Vishnuvardhan S <45058522+vishnuvardhan-s@users.noreply.github.com>
2022-10-26 16:18:36 +0530

    move snippets to powershell script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,54 @@
+# startup stuff
+oh-my-posh init pwsh --config "C:\Users\vishn\AppData\Local\Programs\oh-my-posh\themes\multiverse-neon.omp.json" | Invoke-Expression
+
+# aliases
+New-Alias -Name v -Value nvim
+
+# functions
+function To {
+    param (
+        $Location
+    )
+    if ($Location -eq "one") {
+        Set-Location -Path "E:\Projects\onepiecebounty"
+    }
+    elseif ($Location -eq "dot") {
+        Set-Location -Path "C:\Users\vishn\AppData\Local\nvim"
+    } 
+    elseif ($Location -eq "cat") {
+        Set-Location -Path "E:\Projects\catprep.in"
+    }
+    elseif ($Location -eq "leet") {
+        Set-Location -Path "E:\Projects\leetcode-custom-themes"
+    }
+    elseif ($Location -eq "search") {
+        Set-Location -Path "E:\Projects\search-extension"
+    }
+    elseif ($Location -eq "vim") {
+        Set-Location -Path "E:\Projects\vim-motions"
+    }
+    elseif ($Location -eq "swap") {
+        Set-Location -Path "C:\Users\vishn\AppData\Local\nvim-data\swap"
+    }
+    else {
+        Write-Host ("None was specified, going to Projects folder")
+        Set-Location -Path "E:\Projects"
+    }
+}
+
+function Config {
+    To "dot"
+    nvim init.vim
+}
+
+function gs {
+    git status
+}
+
+function gl {
+    git status --pretty=oneline
+}
+
+function undo {
+    git reset HEAD~1
+}
